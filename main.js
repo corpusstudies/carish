3aa4558cd7a626fa364820f42e5194f673cb7c08
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-04-15 22:11:36 -0700

    Set font before measuring text.
    Use local canvas.

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 "use strict";
-let canvas;
-let context;
 let oldTimeStamp;
-let firstRender = true;
 let windowSize;
 
 window.onload = init;
-window.addEventListener('resize', handleWindowResize);
 
 function init() {
   setWindowSize();
   createCanvas();
-  context = canvas.getContext('2d');
-
+  window.addEventListener('resize', handleWindowResize);
   window.requestAnimationFrame(stepFrame);
 }
 
 function createCanvas() {
-  canvas = document.createElement('canvas');
+  let canvas = document.createElement('canvas');
   canvas.setAttribute('id', 'canvas');
   canvas.setAttribute('width', windowSize.width);
   canvas.setAttribute('height', windowSize.height);
@@ -27,44 +22,49 @@ function createCanvas() {
 
 function handleWindowResize() {
   setWindowSize();
+  let canvas = document.getElementById('canvas');
   canvas.setAttribute('width', windowSize.width);
   canvas.setAttribute('height', windowSize.height);
 }
 
 function setWindowSize() {
   windowSize = { height: window.innerHeight, width: window.innerWidth };
-  console.log(windowSize);
 }
 
 function stepFrame(timeStamp) {
+  let context = document.getElementById('canvas').getContext('2d');
   let fpsText = '';
 
   let millisecondsPassed = timeStamp - oldTimeStamp;
   oldTimeStamp = timeStamp;
 
-  context.fillStyle = 'white';
-  context.fillRect(0, 0, 1000, 800);
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, windowSize.width, windowSize.height);
 
   let fps = Math.round(1000 / millisecondsPassed);
+  if (fps < 0 || isNaN(fps)) {
+    fps = 0;
+  }
+  if (fps > 1000) {
+    fps = 1000;
+  }
 
   fpsText = 'FPS: ' + fps
     + ' Βίβλος γενέσεως Ἰησοῦ '
     + 'בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים';
 
+  context.font = '30px SBL Hebrew';
+  context.fillStyle = 'black';
   let textMetrics = context.measureText(fpsText);
   context.fillStyle = '#ddffdd';
   context.fillRect(
     0, 0,
     Math.abs(textMetrics.actualBoundingBoxLeft) + Math.abs(textMetrics.actualBoundingBoxRight),
     Math.abs(textMetrics.actualBoundingBoxAscent) + Math.abs(textMetrics.actualBoundingBoxDescent));
-  if (firstRender) {
-    console.log(textMetrics);
-    firstRender = false;
-  }
 
   context.font = '30px SBL Hebrew';
   context.fillStyle = 'black';
-  context.fillText(fpsText, 0, textMetrics.actualBoundingBoxAscent);
+  context.fillText(fpsText, 0, Math.abs(textMetrics.actualBoundingBoxAscent));
 
   window.requestAnimationFrame(stepFrame);
 }
